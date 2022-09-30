50c44b52f90942cb56a0719af8d77e7b9bebe32e
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-09-30 00:38:34 +0000

    Update middleware.ts and [...nextauth].js

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname, origin } = req.nextUrl;
 
   if (pathname.startsWith("/api/auth") || token) {
